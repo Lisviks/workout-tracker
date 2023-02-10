35bd1437d2f452b588cd9aeb89f69573a6b5640e
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-02-10 20:26:55 +0000

    temporary profile page

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -11,19 +11,59 @@ class ProfileScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Profile'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            child: const Text('signout'),
-            onPressed: () async {
-              final NavigatorState navState =
-                  context.findAncestorStateOfType<NavigatorState>() ??
-                      Navigator.of(context);
-              await AuthService().signOut();
-              navState.pushNamedAndRemoveUntil('/', (route) => false);
-            },
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 16.0),
+                    child: Text(
+                      'Name',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                  Text(
+                    '${AuthService().user!.displayName}',
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 19.0),
+                    child: Text(
+                      'Email',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                  Text(
+                    '${AuthService().user!.email}',
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('signout'),
+              onPressed: () async {
+                final NavigatorState navState =
+                    context.findAncestorStateOfType<NavigatorState>() ??
+                        Navigator.of(context);
+                await AuthService().signOut();
+                navState.pushNamedAndRemoveUntil('/', (route) => false);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
